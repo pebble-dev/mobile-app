aae09b4bcba55fd7464ec56ce0f69a6df65401db
Lavender Glaab <piggehperson@gmail.com>
2020-06-21 14:42:24 -0400

    Tear out TabBar and replace with a BottomNavigationBar

--- lib/TabsPage.dart
+++ lib/TabsPage.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:fossil/DevOptionsPage.dart';
 import 'package:fossil/setup/PairPage.dart';
+import 'package:fossil/theme.dart';
 
 import 'icons/rebble_icons_stroke_icons.dart';
 import 'maintabs/StoreTab.dart';
@@ -13,83 +14,88 @@ class TabsPage extends StatefulWidget {
 }
 
 class _TabsPageState extends State<TabsPage> {
+  int _currentIndex = 0;
+  List<Widget> _tabs = <Widget>[
+    Column(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: () {},
+          child: Text("Button"),
+        ),
+        Text("This is some text."),
+        Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text("This is a card, with an icon button:"),
+                ],
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.brightness_medium,
+                  color: Color(0xFFF9A285),
+                ),
+                onPressed: () {},
+              )
+            ],
+          ),
+        )
+      ],
+    ), //TODO
+    PairPage(),
+    StoreTab(),
+    Placeholder(), //TODO
+    Placeholder(), //TODO
+  ];
+
+  void _onTabTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 5,
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text("Fossil"),
-              actions: <Widget>[
-                IconButton(icon: Icon(RebbleIconsStroke.devices), onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => PairPage())),),
-                IconButton(icon: Icon(Icons.code), onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => DevOptionsPage())),)
-              ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: CTheme.colorScheme.surface,
+        title: Text("Fossil"),
+        actions: <Widget>[
+          IconButton(icon: Icon(RebbleIconsStroke.developer_connection_console, size: 25.0), onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => DevOptionsPage())),)
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: CTheme.colorScheme.surface,
+          selectedItemColor: CTheme.colorScheme.secondary,
+          type: BottomNavigationBarType.fixed,
+          onTap: _onTabTap,
+          currentIndex: _currentIndex,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(RebbleIconsStroke.send_to_watch_checked, size: 25.0),
+              title: Text("Test"),
+              backgroundColor: CTheme.colorScheme.surface
             ),
-            bottomNavigationBar: BottomAppBar(
-                child: Container(
-                    height: 60,
-                    child: TabBar(
-                      labelColor: Colors.white,
-                      unselectedLabelColor: Color(0x80FFFFFF),
-                      labelPadding: EdgeInsets.all(0.5),
-                      tabs: <Widget>[
-                        Tab(
-                            iconMargin: EdgeInsets.only(bottom: 5.0),
-                            icon: Icon(Icons.crop_square),
-                            text: "Health"),
-                        Tab(
-                            iconMargin: EdgeInsets.only(bottom: 5.0),
-                            icon: Icon(Icons.crop_square),
-                            text: "Locker"),
-                        Tab(
-                            iconMargin: EdgeInsets.only(bottom: 5.0),
-                            icon: Icon(Icons.crop_square),
-                            text: "Store"),
-                        Tab(
-                            iconMargin: EdgeInsets.only(bottom: 5.0),
-                            icon: Icon(Icons.crop_square),
-                            text: "Notifications"),
-                        Tab(
-                            iconMargin: EdgeInsets.only(bottom: 5.0),
-                            icon: Icon(Icons.crop_square),
-                            text: "More"),
-                      ],
-                    ))),
-            body: TabBarView(
-              children: <Widget>[
-                Column(
-                    children: <Widget>[
-                      RaisedButton(
-                        onPressed: () {},
-                        child: Text("Button"),
-                      ),
-                      Text("This is some text."),
-                      Card(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Row(
-                              children: <Widget>[
-                                Text("This is a card, with an icon button:"),
-                              ],
-                            ),
-                            IconButton(
-                              icon: Icon(
-                                Icons.brightness_medium,
-                                color: Color(0xFFF9A285),
-                              ),
-                              onPressed: () {},
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ), //TODO
-                Scaffold(), //TODO
-                StoreTab(),
-                Scaffold(), //TODO
-                Scaffold(), //TODO
-              ],
-            )));
+            BottomNavigationBarItem(
+              icon: Icon(RebbleIconsStroke.devices, size: 25.0),
+              title: Text("Devices"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(RebbleIconsStroke.rebble_store, size: 25.0),
+              title: Text("Store"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(RebbleIconsStroke.notifications, size: 25.0),
+              title: Text("Notifications"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(RebbleIconsStroke.menu_horizontal, size: 25.0),
+              title: Text("More"),
+            ),
+          ]),
+      body: _tabs[_currentIndex],
+    );
   }
 }
