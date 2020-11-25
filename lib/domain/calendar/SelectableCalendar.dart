3744690cbc54e74aca17fe0baaa0f29662e814e6
Matej Drobnic <matejdro@gmail.com>
2020-11-25 21:02:00 +0100

    add equals and string methods to SelectableCalendar

--- lib/domain/calendar/SelectableCalendar.dart
+++ lib/domain/calendar/SelectableCalendar.dart
@@ -4,4 +4,21 @@ class SelectableCalendar {
   bool enabled;
 
   SelectableCalendar(this.name, this.id, this.enabled);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SelectableCalendar &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          id == other.id &&
+          enabled == other.enabled;
+
+  @override
+  int get hashCode => name.hashCode ^ id.hashCode ^ enabled.hashCode;
+
+  @override
+  String toString() {
+    return 'SelectableCalendar{name: $name, id: $id, enabled: $enabled}';
+  }
 }
