201d67e7f5e57a8e63d320c75006a02466d71597
Alice Grey <alice@grey.systems>
2020-12-04 15:42:40 -0600

    Add divider and 2nd list of watches
    Add divider and 2nd list of watches(currently just shows the same watches twice)

--- lib/ui/home/tabs/watches_tab.dart
+++ lib/ui/home/tabs/watches_tab.dart
@@ -76,7 +76,60 @@ class _MyWatchesTabState extends State<MyWatchesTab> {
                     ]),
                     margin: EdgeInsets.all(16)),
                 onTap: () {
+                },
+              )
 
+              ).toList()
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(15, 25, 15, 5),
+              child: Text('All watches',
+                  style: TextStyle(fontSize: 18))),
+          const Divider(
+            color: Colors.white24,
+            height: 20,
+            thickness: 2,
+            indent: 15,
+            endIndent: 15,
+          ),
+          Column(
+
+              children: _pebbles
+                  .map((e) => InkWell(
+                child: Container(
+                    child: Row(children: <Widget>[
+                      Container(
+                        child: Center(
+                            child: PebbleWatchIcon(
+                                PebbleWatchModel.values[e.color])),
+                        width: 56,
+                        height: 56,
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).dividerColor,
+                            shape: BoxShape.circle),
+                      ),
+                      SizedBox(width: 16),
+                      Column(
+                        children: <Widget>[
+                          Text(e.name, style: TextStyle(fontSize: 16)),
+                          SizedBox(height: 4),
+                          Text(e.address
+                              .toRadixString(16)
+                              .padLeft(6, '0')
+                              .toUpperCase()),
+                          Wrap(
+                            spacing: 4,
+                            children: [
+                            ],
+                          ),
+                        ],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                      ),
+                      Expanded(
+                          child: Container(width: 0.0, height: 0.0)),
+                    ]),
+                    margin: EdgeInsets.fromLTRB(16, 10, 16, 16),),
+                onTap: () {
                 },
               )).toList()),
         ]),
