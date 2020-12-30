c2fe58c9f3eaad4fdabc91772ebf949df98b00a1
Matic Leva <vertical3life@gmail.com>
2020-12-30 22:05:03 +0100

    Add custom theming for buttons

---
+++ lib/ui/common/components/cobble_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+/// Simple Cobble button.
+///
+/// [onPressed] parameter is required but can be null. At least one of [label]
+/// and [icon] need be defined.
+class CobbleButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final FocusNode focusNode;
+  final String label;
+  final IconData icon;
+  final bool outlined;
+
+  const CobbleButton({
+    Key key,
+    @required this.onPressed,
+    this.label,
+    this.icon,
+    this.outlined = true,
+    this.focusNode,
+  })  : assert(
+          label is String && label.length > 0 || icon is IconData,
+          "CobbleButton needs label and/or icon",
+        ),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final child = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (icon is IconData) Icon(icon, size: 21),
+        if (icon is IconData && label is String) SizedBox(width: 8),
+        if (label is String) Text(label.toUpperCase()),
+      ],
+    );
+    if (outlined)
+      return OutlinedButton(
+        onPressed: onPressed,
+        focusNode: focusNode,
+        child: child,
+      );
+    return TextButton(
+      onPressed: onPressed,
+      focusNode: focusNode,
+      child: child,
+    );
+  }
+}
