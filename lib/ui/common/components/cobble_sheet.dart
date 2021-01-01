4c2820712be050a154b9e3676b59e77cc692db7f
Matic Leva <vertical3life@gmail.com>
2021-01-01 13:25:10 +0100

    Implement custom Cobble bottom sheet

---
+++ lib/ui/common/components/cobble_sheet.dart
@@ -0,0 +1,71 @@
+import 'package:cobble/ui/theme/with_cobble_theme.dart';
+import 'package:flutter/material.dart';
+
+/// Shows a modal Cobble design bottom sheet.
+///
+/// A modal bottom sheet prevents the user from interacting with the rest of
+/// the app until she dismisses it.
+///
+/// The [context] argument is used to look up the [Navigator] and [Theme] for
+/// the bottom sheet. It is only used when the method is called. Its
+/// corresponding widget can be safely removed from the tree before the bottom
+/// sheet is closed. Inside [builder] function you should use [context] provided
+/// to builder instead of one passed to [showCobbleSheet].
+Future<void> showCobbleSheet({
+  @required BuildContext context,
+  @required WidgetBuilder builder,
+  bool dismissible = true,
+}) async {
+  final borderRadius = BorderRadius.vertical(
+    top: Radius.circular(4),
+    bottom: Radius.zero,
+  );
+  await showModalBottomSheet(
+    context: context,
+    useRootNavigator: true,
+    shape: RoundedRectangleBorder(
+      borderRadius: borderRadius,
+    ),
+    builder: (context) {
+      return Container(
+        decoration: BoxDecoration(
+          color: context.scheme.elevated,
+          borderRadius: borderRadius,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 8),
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 132,
+                    child: Container(),
+                  ),
+                  Flexible(
+                    flex: 96,
+                    child: Container(
+                      height: 4,
+                      decoration: BoxDecoration(
+                        color: context.scheme.divider,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(2),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 132,
+                    child: Container(),
+                  ),
+                ],
+              ),
+            ),
+            builder(context),
+          ],
+        ),
+      );
+    },
+  );
+}
