38393249ca31471de89b3be3de6e3bea3140b7b9
Matej Drobnic <matejdro@gmail.com>
2021-04-07 14:37:12 +0200

    move background calendar flutter logic to separate file

--- lib/background/main_background.dart
+++ lib/background/main_background.dart
@@ -1,17 +1,13 @@
 import 'package:cobble/background/notification/notification_manager.dart';
 import 'package:cobble/domain/app_lifecycle_manager.dart';
-import 'package:cobble/domain/calendar/calendar_pin_convert.dart';
-import 'package:cobble/domain/calendar/calendar_syncer.db.dart';
 import 'package:cobble/domain/connection/connection_state_provider.dart';
 import 'package:cobble/domain/db/dao/app_dao.dart';
-import 'package:cobble/domain/db/dao/timeline_pin_dao.dart';
 import 'package:cobble/domain/db/models/app.dart';
 import 'package:cobble/domain/db/models/next_sync_action.dart';
 import 'package:cobble/domain/entities/pbw_app_info_extension.dart';
 import 'package:cobble/domain/entities/pebble_device.dart';
 import 'package:cobble/domain/logging.dart';
 import 'package:cobble/domain/timeline/watch_apps_syncer.dart';
-import 'package:cobble/domain/timeline/watch_timeline_syncer.dart';
 import 'package:cobble/infrastructure/datasources/preferences.dart';
 import 'package:cobble/infrastructure/pigeons/pigeons.g.dart';
 import 'package:cobble/util/container_extensions.dart';
@@ -21,6 +17,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:uuid_type/uuid_type.dart';
 
 import 'actions/master_action_handler.dart';
+import 'modules/calendar_background.dart';
 
 void main_background() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,16 +27,15 @@ void main_background() {
 
 class BackgroundReceiver
     implements
-        CalendarCallbacks,
         TimelineCallbacks,
         NotificationListening,
         BackgroundAppInstallCallbacks {
   final container = ProviderContainer();
-  late CalendarSyncer calendarSyncer;
-  late WatchTimelineSyncer watchTimelineSyncer;
+
+  late CalendarBackground calendarBackground;
+
+  late Future<Preferences> preferences;
   late WatchAppsSyncer watchAppsSyncer;
-  Future<Preferences>? preferences;
-  late TimelinePinDao timelinePinDao;
   late MasterActionHandler masterActionHandler;
   late NotificationManager notificationManager;
   late AppLifecycleManager appLifecycleManager;
@@ -54,11 +50,8 @@ class BackgroundReceiver
   void init() async {
     await BackgroundControl().notifyFlutterBackgroundStarted();
 
-    calendarSyncer = container.listen(calendarSyncerProvider!).read();
     notificationManager = container.listen(notificationManagerProvider).read();
-    watchTimelineSyncer = container.listen(watchTimelineSyncerProvider!).read();
     watchAppsSyncer = container.listen(watchAppSyncerProvider).read();
-    timelinePinDao = container.listen(timelinePinDaoProvider!).read();
     appDao = container.listen(appDaoProvider).read();
     appLifecycleManager = container.listen(appLifecycleManagerProvider).read();
     preferences = Future.microtask(() async {
@@ -70,7 +63,7 @@ class BackgroundReceiver
     masterActionHandler = container.read(masterActionHandlerProvider);
 
     connectionSubscription = container.listen(
-      connectionStateProvider!.state,
+      connectionStateProvider.state,
       mayHaveChanged: (sub) {
         final currentConnectedWatch = sub.read().currentConnectedWatch;
         if (isConnectedToWatch()! && currentConnectedWatch!.name!.isNotEmpty) {
@@ -79,51 +72,42 @@ class BackgroundReceiver
       },
     );
 
-    CalendarCallbacks.setup(this);
     TimelineCallbacks.setup(this);
     NotificationListening.setup(this);
     BackgroundAppInstallCallbacks.setup(this);
-  }
 
-  @override
-  Future<void> doFullCalendarSync() async {
-    await calendarSyncer.syncDeviceCalendarsToDb();
-    await syncTimelineToWatch();
+    calendarBackground = CalendarBackground(this.container);
+    calendarBackground.init();
   }
 
   void onWatchConnected(PebbleDevice watch) async {
     final lastConnectedWatch =
-        (await preferences)!.getLastConnectedWatchAddress();
+        (await preferences).getLastConnectedWatchAddress();
+    bool unfaithful = false;
     if (lastConnectedWatch != watch.address) {
-      Log.d("Different watch connected than the last one. Resetting DB...");
-      await watchTimelineSyncer.clearAllPinsFromWatchAndResync();
+      Log.d("Different watch connected than the last one.");
+      unfaithful = true;
     } else if (watch.isUnfaithful!) {
-      Log.d("Connected watch has beein unfaithful (tsk, tsk tsk). Reset DB...");
-      await watchTimelineSyncer.clearAllPinsFromWatchAndResync();
-    } else {
-      await syncTimelineToWatch();
-      Log.d('Watch connected');
-      await watchAppsSyncer.syncAppDatabaseWithWatch();
+      Log.d("Connected watch has beein unfaithful (tsk, tsk tsk)");
+      unfaithful = true;
     }
 
-    (await preferences)!.setLastConnectedWatchAddress(watch.address!);
+    await calendarBackground.onWatchConnected(watch, unfaithful);
+    await watchAppsSyncer.syncAppDatabaseWithWatch();
+
+    Log.d('Watch connected');
+
+    (await preferences).setLastConnectedWatchAddress(watch.address!);
   }
 
   Future syncTimelineToWatch() async {
-    if (isConnectedToWatch()!) {
-      await watchTimelineSyncer.syncPinDatabaseWithWatch();
-    }
+    await calendarBackground.syncTimelineToWatch();
   }
 
   bool? isConnectedToWatch() {
     return connectionSubscription.read().isConnected;
   }
 
-  @override
-  Future<void> deleteCalendarPinsFromWatch() async {
-    await timelinePinDao.markAllPinsFromAppForDeletion(calendarWatchappId);
-    await syncTimelineToWatch();
-  }
 
   @override
   Future<ActionResponsePigeon> handleTimelineAction(ActionTrigger arg) async {
@@ -161,7 +145,7 @@ class BackgroundReceiver
     await appDao.insertOrUpdateApp(newApp);
 
     final blobDbSyncSuccess = await watchAppsSyncer.syncAppDatabaseWithWatch();
-    Log.d("Blob sync success: ${blobDbSyncSuccess}");
+    Log.d("Blob sync success: $blobDbSyncSuccess");
 
     if (blobDbSyncSuccess) {
       await appLifecycleManager.openApp(newApp.uuid);
---
+++ lib/background/modules/calendar_background.dart
@@ -0,0 +1,65 @@
+import 'package:cobble/domain/calendar/calendar_pin_convert.dart';
+import 'package:cobble/domain/calendar/calendar_syncer.db.dart';
+import 'package:cobble/domain/connection/connection_state_provider.dart';
+import 'package:cobble/domain/db/dao/timeline_pin_dao.dart';
+import 'package:cobble/domain/entities/pebble_device.dart';
+import 'package:cobble/domain/logging.dart';
+import 'package:cobble/domain/timeline/watch_timeline_syncer.dart';
+import 'package:cobble/infrastructure/pigeons/pigeons.g.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class CalendarBackground implements CalendarCallbacks {
+  final ProviderContainer container;
+
+  late CalendarSyncer calendarSyncer;
+  late WatchTimelineSyncer watchTimelineSyncer;
+  late TimelinePinDao timelinePinDao;
+
+  late ProviderSubscription<WatchConnectionState> connectionSubscription;
+
+  CalendarBackground(this.container);
+
+  void init() async {
+    calendarSyncer = container.listen(calendarSyncerProvider).read();
+    watchTimelineSyncer = container.listen(watchTimelineSyncerProvider).read();
+    timelinePinDao = container.listen(timelinePinDaoProvider).read();
+
+    CalendarCallbacks.setup(this);
+
+    connectionSubscription = container.listen(
+      connectionStateProvider.state,
+    );
+  }
+
+  Future<void> onWatchConnected(PebbleDevice watch, bool unfaithful) async {
+    if (unfaithful) {
+      Log.d("Clearing all pins");
+      await watchTimelineSyncer.clearAllPinsFromWatchAndResync();
+    } else {
+      Log.d('Performing normal calendar sync');
+      await syncTimelineToWatch();
+    }
+  }
+
+  @override
+  Future<void> deleteCalendarPinsFromWatch() async {
+    await timelinePinDao.markAllPinsFromAppForDeletion(calendarWatchappId);
+    await syncTimelineToWatch();
+  }
+
+  @override
+  Future<void> doFullCalendarSync() async {
+    await calendarSyncer.syncDeviceCalendarsToDb();
+    await syncTimelineToWatch();
+  }
+
+  Future syncTimelineToWatch() async {
+    if (isConnectedToWatch()!) {
+      await watchTimelineSyncer.syncPinDatabaseWithWatch();
+    }
+  }
+
+  bool? isConnectedToWatch() {
+    return connectionSubscription.read().isConnected;
+  }
+}
