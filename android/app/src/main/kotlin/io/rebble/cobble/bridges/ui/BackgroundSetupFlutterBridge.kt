406dcdf6d910180826252400f32be41d40ea316a
Matej Drobnic <matejdro@gmail.com>
2021-06-21 19:14:08 +0200

    start background flutter immediately after opening app

--- android/app/src/main/kotlin/io/rebble/cobble/bridges/ui/BackgroundSetupFlutterBridge.kt
+++ android/app/src/main/kotlin/io/rebble/cobble/bridges/ui/BackgroundSetupFlutterBridge.kt
@@ -1,13 +1,18 @@
 package io.rebble.cobble.bridges.ui
 
 import io.rebble.cobble.bridges.FlutterBridge
+import io.rebble.cobble.bridges.background.FlutterBackgroundController
 import io.rebble.cobble.datasources.AndroidPreferences
 import io.rebble.cobble.pigeons.Pigeons
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class BackgroundSetupFlutterBridge @Inject constructor(
         bridgeLifecycleController: BridgeLifecycleController,
-        private val androidPreferences: AndroidPreferences
+        private val androidPreferences: AndroidPreferences,
+        private val flutterBackgroundController: FlutterBackgroundController
 ) : FlutterBridge, Pigeons.BackgroundSetupControl {
     init {
         bridgeLifecycleController.setupControl(Pigeons.BackgroundSetupControl::setup, this)
@@ -15,6 +20,10 @@ class BackgroundSetupFlutterBridge @Inject constructor(
 
     override fun setupBackground(arg: Pigeons.NumberWrapper) {
         androidPreferences.backgroundEndpoint = arg.value
+
+        GlobalScope.launch(Dispatchers.Main.immediate) {
+            flutterBackgroundController.getBackgroundFlutterEngine()
+        }
     }
 
 }
