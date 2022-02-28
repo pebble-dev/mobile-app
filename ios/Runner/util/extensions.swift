30bfd51eb4dfcc06fc9d3ce1ed7b7746ee1ef4fb
crc-32 <me@crc32.dev>
2022-02-28 22:48:00 +0000

    more promise extensions

--- ios/Runner/util/extensions.swift
+++ ios/Runner/util/extensions.swift
@@ -38,7 +38,63 @@ extension Kotlinx_coroutines_coreChannelIterator {
 extension PutBytesService {
     func sendPromise(packet: PutBytesOutgoingPacket) -> Promise<KotlinUnit> {
         return Promise { seal in
-            self.send(packet: packet, completionHandler: kotlinSuspendResolver(seal: seal))
+            DispatchQueue.main.async {
+                self.send(packet: packet, completionHandler: kotlinSuspendResolver(seal: seal))
+            }
+        }
+    }
+}
+
+extension SystemService {
+    func requestWatchModelPromise() -> Promise<Int> {
+        return Promise { seal in
+            DispatchQueue.main.async {
+                self.requestWatchModel(completionHandler: kotlinSuspendResolver(seal: seal))
+            }
+        }.map { $0.intValue }
+    }
+    
+    func requestWatchVersionPromise() -> Promise<WatchVersion.WatchVersionResponse> {
+        return Promise { seal in
+            DispatchQueue.main.async {
+                self.requestWatchVersion(completionHandler: kotlinSuspendResolver(seal: seal))
+            }
+        }
+    }
+    
+    func sendPromise(packet: SystemPacket, priority: PacketPriority) -> Promise<Void> {
+        return Promise { seal in
+            DispatchQueue.main.async {
+                self.send(packet: packet, priority: priority, completionHandler: kotlinSuspendResolver(seal: seal))
+            }
+        }.map { _ in}
+    }
+}
+
+//MARK: - Misc. extensions
+
+extension ProtocolHandlerImpl {
+    func openProtocolPromise() -> Promise<Void> {
+        return Promise { seal in
+            DispatchQueue.main.async {
+                self.openProtocol(completionHandler: kotlinSuspendResolver(seal: seal))
+            }
+        }.map { _ in }
+    }
+    
+    func closeProtocolPromise() -> Promise<Void> {
+        return Promise { seal in
+            DispatchQueue.main.async {
+                self.closeProtocol(completionHandler: kotlinSuspendResolver(seal: seal))
+            }
+        }.map { _ in }
+    }
+    
+    func waitForNextPacketPromise() -> Promise<ProtocolHandlerPendingPacket> {
+        return Promise { seal in
+            DispatchQueue.main.async {
+                self.waitForNextPacket(completionHandler: kotlinSuspendResolver(seal: seal))
+            }
         }
     }
 }
