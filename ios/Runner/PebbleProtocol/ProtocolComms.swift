80288e129e539ab0337956e459a71e9e6a7d24c8
crc-32 <me@crc32.dev>
2022-02-28 22:46:41 +0000

    fix libpebblecommon packet piping

--- ios/Runner/PebbleProtocol/ProtocolComms.swift
+++ ios/Runner/PebbleProtocol/ProtocolComms.swift
@@ -8,12 +8,14 @@
 import Foundation
 import libpebblecommon
 import CocoaLumberjackSwift
+import PromiseKit
+
 class ProtocolComms {
     static var shared: ProtocolComms!
     
     private let protocolHandler = ProtocolHandlerImpl()
     
-    private let rawSend = RawSend()
+    //private let rawSend = RawSend()
     
     public let systemService: SystemService
     public let appFetchService: AppFetchService
@@ -22,6 +24,9 @@ class ProtocolComms {
     public let systemHandler: SystemHandler
     public let appInstallHandler: AppInstallHandler
     public let putBytesController: PutBytesController
+    
+    private let ioDispatch = DispatchQueue(label: Bundle.main.bundleIdentifier!+".ProtocolCommsIO", qos: .default)
+    private var sendLoopRunning = false
     
     init() {
         systemService = SystemService(protocolHandler: protocolHandler)
@@ -34,10 +39,11 @@ class ProtocolComms {
         putBytesController = PutBytesController(putBytesService: putBytesService)
     }
     
-    private class RawSend: KotlinSuspendFunction1 {
+    /*private class RawSend: KotlinSuspendFunction1 {
         func invoke(p1: Any?, completionHandler: @escaping (Any?, Error?) -> Void) {
             let bytearray = KUtil.shared.uByteArrayAsByteArray(arr: p1!)
             do {
+                DDLogDebug("WRITING: \(bytearray.size)")
                 try LEPeripheral.shared.writePacket(rawProtocolPacket: [UInt8](bytearray.toNative()))
             }catch is PPoGATTServiceNotInitialized {
                 DDLogWarn("Tried to send packet before GATT was init'd")
@@ -51,6 +57,20 @@ class ProtocolComms {
             }
             completionHandler(true, nil)
         }
+    }*/
+    
+    private func rawSend(bytearray: KotlinByteArray) -> Bool {
+        do {
+            try LEPeripheral.shared.writePacket(rawProtocolPacket: [UInt8](bytearray.toNative()))
+            return true
+        }catch is PPoGATTServiceNotInitialized {
+            DDLogWarn("Tried to send packet before GATT was init'd")
+            assertionFailure()
+            return false
+        }catch {
+            DDLogError(error)
+            return false
+        }
     }
     
     public func receivePacket(packet: [UInt8]) {
@@ -60,13 +80,17 @@ class ProtocolComms {
         }
         let ubytes = KUtil.shared.byteArrayAsUByteArray(arr: bytes)
         
-        protocolHandler.receivePacket(bytes: ubytes) {_, _ in }
+        DispatchQueue.main.async {
+            self.protocolHandler.receivePacket(bytes: ubytes) {_, _ in }
+        }
     }
     
-    public func sendPacket(packet: PebblePacket, completionHandler: @escaping (Bool, Error?) -> Void) {
-        DispatchQueue.main.async {[self] in
-            protocolHandler.send(packet: packet, priority: .normal) { success, err in
-                completionHandler(success?.boolValue ?? false, err)
+    public func sendPacket(packet: PebblePacket) -> Promise<Bool> {
+        return Promise { seal in
+            DispatchQueue.main.async {[self] in
+                protocolHandler.send(packet: packet, priority: .normal) { success, err in
+                    seal.resolve(success?.boolValue, err)
+                }
             }
         }
     }
@@ -74,20 +98,49 @@ class ProtocolComms {
     @available(iOS 13.0.0, *)
     public func sendPacketAsync(packet: PebblePacket) async throws -> Bool {
         return try await withCheckedThrowingContinuation { continuation in
-            sendPacket(packet: packet) { result, error in
-                if let error = error {
-                    continuation.resume(throwing: error)
-                    return
-                }
+            sendPacket(packet: packet).done { result in
                 continuation.resume(returning: result)
+            }.catch { error in
+                continuation.resume(throwing: error)
             }
         }
     }
     
     public func startPacketSendingLoop() {
+        guard !sendLoopRunning else {
+            DDLogWarn("Ignored call on startPacketSendingLoop(): Already running")
+            return
+        }
+        sendLoopRunning = true
         DispatchQueue.main.async {[self] in
-            protocolHandler.startPacketSendingLoop(rawSend: rawSend) { _, e in
+            firstly {
+                protocolHandler.openProtocolPromise()
+            }.done {
+                ioDispatch.async {
+                    defer {
+                        protocolHandler.closeProtocolPromise().cauterize()
+                        sendLoopRunning = false
+                    }
+                    while (true) {
+                        do {
+                            let packet = try protocolHandler.waitForNextPacketPromise().wait()
+                            let success = rawSend(bytearray: KUtil.shared.uByteArrayAsByteArray(arr: packet.data))
+                            
+                            if (success) {
+                                packet.notifyPacketStatus(success: true)
+                            }else {
+                                packet.notifyPacketStatus(success: false)
+                                break
+                            }
+                        } catch {
+                            DDLogError("Error in sending loop: \(error)")
+                            break
+                        }
+                    }
+                }
                 DDLogInfo("Sending loop ended")
+            }.catch { error in
+                sendLoopRunning = false
             }
         }
     }
