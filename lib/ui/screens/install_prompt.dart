04dc05baba1d7deb75f7f4ca85865c94c55514d1
crc-32 <me@crc32.dev>
2022-03-06 00:54:43 +0000

    ios packages resolved

--- lib/ui/screens/install_prompt.dart
+++ lib/ui/screens/install_prompt.dart
@@ -7,7 +7,6 @@ import 'package:cobble/domain/entities/pebble_device.dart';
 import 'package:cobble/infrastructure/pigeons/pigeons.g.dart';
 import 'package:cobble/ui/router/cobble_scaffold.dart';
 import 'package:cobble/ui/router/cobble_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
