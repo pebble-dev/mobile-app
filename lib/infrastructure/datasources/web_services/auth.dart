5e358c893fcca1fe2c8df8dbfb2164b5bea37ce5
crc-32 <me@crc32.dev>
2022-06-19 02:51:12 +0100

    use oauth provider in rws auth

--- lib/infrastructure/datasources/web_services/auth.dart
+++ lib/infrastructure/datasources/web_services/auth.dart
@@ -1,14 +1,27 @@
 import 'dart:async';
 
+import 'package:cobble/domain/api/auth/oauth.dart';
+import 'package:cobble/domain/api/auth/oauth_token.dart';
 import 'package:cobble/domain/api/auth/user.dart';
+import 'package:cobble/infrastructure/datasources/preferences.dart';
 import 'package:cobble/infrastructure/datasources/web_services/service.dart';
 
 class AuthService extends Service {
-  AuthService(String baseUrl, this._token) : super(baseUrl);
-  final String _token;
+  static const String version = "v1";
+  AuthService(String baseUrl, this._prefs, this._oauth, this._token)
+      : super(baseUrl + "/" + version);
+  final OAuthToken _token;
+  final OAuthClient _oauth;
+  final Preferences _prefs;
 
   Future<User> get user async {
-    User user = await client.getSerialized(User.fromJson, "me", token: _token);
+    final tokenCreationDate = _prefs.getOAuthTokenCreationDate();
+    if (tokenCreationDate == null) {
+      throw StateError("token creation date null when token exists");
+    }
+    final token = await _oauth.ensureNotStale(_token, tokenCreationDate);
+    User user = await client.getSerialized(User.fromJson, "me",
+        token: token.accessToken);
     return user;
   }
 }
