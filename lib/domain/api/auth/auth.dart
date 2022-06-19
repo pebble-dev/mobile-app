d85a397a4ee6fe5fe4c0e21b80c385a3df4286e3
crc-32 <me@crc32.dev>
2022-06-19 02:51:43 +0100

    use oauth provider in auth provider

--- lib/domain/api/auth/auth.dart
+++ lib/domain/api/auth/auth.dart
@@ -1,4 +1,6 @@
+import 'package:cobble/domain/api/auth/oauth.dart';
 import 'package:cobble/domain/api/boot/boot.dart';
+import 'package:cobble/infrastructure/datasources/preferences.dart';
 import 'package:cobble/infrastructure/datasources/secure_storage.dart';
 import 'package:cobble/infrastructure/datasources/web_services/auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -6,8 +8,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final authServiceProvider = Provider((ref) async {
   final boot = await (await ref.watch(bootServiceProvider.future)).config;
   final token = await (await ref.watch(tokenProvider.last));
+  final oauth = await ref.watch(oauthClientProvider.future);
+  final prefs = await ref.watch(preferencesProvider.future);
   if (token == null) {
     throw StateError("Service requires a token but none was found in storage");
   }
-  return AuthService(boot.auth.base, token);
+  return AuthService(boot.auth.base, prefs, oauth, token);
 });
