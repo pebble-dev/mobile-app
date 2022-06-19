81e9135faab3248c3aa3d5a17aa55a8f1642764f
crc-32 <me@crc32.dev>
2022-06-19 02:53:16 +0100

    clean up splash

--- lib/ui/splash/splash_page.dart
+++ lib/ui/splash/splash_page.dart
@@ -1,7 +1,4 @@
 import 'package:cobble/infrastructure/datasources/preferences.dart';
-import 'package:cobble/localization/localization.dart';
-import 'package:cobble/main.dart';
-import 'package:cobble/ui/common/components/cobble_button.dart';
 import 'package:cobble/ui/home/home_page.dart';
 import 'package:cobble/ui/router/cobble_navigator.dart';
 import 'package:cobble/ui/router/cobble_scaffold.dart';
@@ -9,9 +6,10 @@ import 'package:cobble/ui/setup/first_run_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class SplashPage extends HookWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
   void Function() _openHome(
     bool hasBeenConnected, {
     required BuildContext context,
@@ -24,38 +22,6 @@ class SplashPage extends HookWidget {
         }
       };
 
-  // ignore: unused_element
-  void _askToBoot(bool hasBeenConnected, {required BuildContext context}) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          final openHome = _openHome(hasBeenConnected, context: context);
-          return AlertDialog(
-            title: Text(tr.splashPage.title),
-            content: Text(tr.splashPage.body),
-            actions: <Widget>[
-              CobbleButton(
-                outlined: false,
-                label: tr.common.yes,
-                onPressed: () {
-                  canLaunch(getBootUrl).then((value) {
-                    if (value)
-                      launch(getBootUrl).then((_) => openHome());
-                    else
-                      openHome();
-                  });
-                },
-              ),
-              CobbleButton(
-                outlined: false,
-                label: tr.common.no,
-                onPressed: openHome,
-              ),
-            ],
-          );
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
     final hasBeenConnected = useProvider(hasBeenConnectedProvider).data;
@@ -67,7 +33,7 @@ class SplashPage extends HookWidget {
       }
     }, [hasBeenConnected]);
     return CobbleScaffold.page(
-      child: Center(
+      child: const Center(
         // This page shouldn't be visible for more than a split second, but if
         // it ever is, let the user know it's not broken
         child: CircularProgressIndicator(),
