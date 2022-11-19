3a3e22ff9c88edd3cf5a7f9ffa7207cd15e55fa1
crc-32 <me@crc32.dev>
2022-11-19 19:58:40 +0000

    fix off-by-one

--- android/app/src/main/kotlin/io/rebble/cobble/bluetooth/gatt/PendingPacket.kt
+++ android/app/src/main/kotlin/io/rebble/cobble/bluetooth/gatt/PendingPacket.kt
@@ -8,23 +8,22 @@ class PendingPacket() {
     private var pendingLength: Int? = null
     private val packetBuf = mutableListOf<Byte>()
 
-    public val data: List<Byte> = packetBuf
+    val data: List<Byte> = packetBuf
 
-    public val isComplete get() = pendingLength == 0
+    val isComplete get() = pendingLength == 0
 
     companion object {
         private fun getPacketLength(packet: List<Byte>): Int {
             val headBuf = DataBuffer(packet.toByteArray().asUByteArray())
-            val length = headBuf.getUShort()
-            return length.toInt()
+            return headBuf.getUShort().toInt()
         }
     }
 
-    public fun addData(data: List<Byte>): Int {
+    fun addData(data: List<Byte>): Int {
         if (pendingLength == null) {
             pendingLength = getPacketLength(data)+4
         }
-        val toAdd = data.subList(0, min(pendingLength!!-1, data.lastIndex))
+        val toAdd = data.subList(0, min(pendingLength!!, data.size))
         packetBuf.addAll(toAdd)
         pendingLength = pendingLength!! - toAdd.size
         return toAdd.size
