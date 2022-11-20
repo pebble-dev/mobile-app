e337b59484d203aca1b27aa5041004c679d44082
crc-32 <me@crc32.dev>
2022-11-20 03:24:33 +0000

    test watch phoneversion handshake

--- android/app/src/test/java/io/rebble/cobble/bluetooth/gatt/PPoGATTProtocolHandlerTest.kt
+++ android/app/src/test/java/io/rebble/cobble/bluetooth/gatt/PPoGATTProtocolHandlerTest.kt
@@ -1,24 +1,18 @@
 package io.rebble.cobble.bluetooth.gatt
 
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothGatt
-import android.bluetooth.BluetoothGattCallback
-import android.bluetooth.BluetoothGattCharacteristic
-import android.bluetooth.BluetoothGattDescriptor
-import android.bluetooth.BluetoothGattServer
-import android.bluetooth.BluetoothGattServerCallback
-import android.bluetooth.BluetoothGattService
-import android.bluetooth.BluetoothManager
+import android.bluetooth.*
 import android.content.Context
 import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import io.rebble.libpebblecommon.ProtocolHandlerImpl
 import io.rebble.libpebblecommon.ble.GATTPacket
 import io.rebble.libpebblecommon.ble.LEConstants
+import io.rebble.libpebblecommon.packets.PhoneAppVersion
 import io.rebble.libpebblecommon.protocolhelpers.PebblePacket
-import io.rebble.libpebblecommon.util.runBlocking
+import io.rebble.libpebblecommon.protocolhelpers.ProtocolEndpoint
+import io.rebble.libpebblecommon.util.DataBuffer
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.test.*
@@ -27,7 +21,6 @@ import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.mockito.*
 import org.mockito.ArgumentMatchers.any
-import org.mockito.ArgumentMatchers.anyCollection
 import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 import timber.log.Timber
@@ -54,6 +47,10 @@ class PPoGATTProtocolHandlerTest {
 
     private val charValues = mutableMapOf<UUID, ByteArray?>()
 
+    private val dataUpdates = Channel<ByteArray>(Channel.UNLIMITED)
+
+    var seq = 0
+
     @Before
     fun setUp() {
         MockitoAnnotations.openMocks(this)
@@ -74,6 +71,9 @@ class PPoGATTProtocolHandlerTest {
             `when`(mock.getValue()).thenReturn(charValues[uuid])
             `when`(mock.setValue(any(ByteArray::class.java))).then {
                 charValues[uuid] = it.getArgument<ByteArray>(0)
+                if (UUID.fromString(LEConstants.UUIDs.PPOGATT_DEVICE_CHARACTERISTIC_SERVER) == uuid) {
+                    dataUpdates.trySend(charValues[UUID.fromString(LEConstants.UUIDs.PPOGATT_DEVICE_CHARACTERISTIC_SERVER)]!!)
+                }
                 return@then true
             }
         })
@@ -105,6 +105,7 @@ class PPoGATTProtocolHandlerTest {
         `when`(gatt.device).thenReturn(device)
         `when`(device.address).thenReturn("12:23:34:98:76:54")
         `when`(device.connectGatt(any(Context::class.java), anyBoolean(), any(BluetoothGattCallback::class.java), anyInt())).thenReturn(gatt)
+        seq = 0
     }
 
     @After
@@ -128,6 +129,69 @@ class PPoGATTProtocolHandlerTest {
         val defer = async {
             protocolHandler.connectionStateChannel.receive()
         }
+        watchConnection()
+        watchSendPacket(byteArrayOf(0), 139)
+        advanceUntilIdle()
+
+        assert(defer.await())
+    }
+
+    @Test
+    fun `Watch handshake with libpebblecommon`() = runTest {
+        val scope = CoroutineScope(testScheduler)
+        val leServer = PPoGATTServer(context, scope)
+        val protocolHandler = PPoGATTProtocolHandler(scope, leServer)
+        val pebbleProtocol = ProtocolHandlerImpl()
+        val resp = PhoneAppVersion.AppVersionResponse(
+                1u,
+                1u,
+                1u,
+                1u,
+                1u,
+                1u,
+                1u,
+                ubyteArrayOf(1u)
+        )
+        protocolHandler.rxPebblePacketFlow.onEach {
+            Timber.d("RX ${PebblePacket.deserialize(it.asUByteArray())::class.simpleName}")
+            pebbleProtocol.receivePacket(it.asUByteArray())
+        }.launchIn(scope)
+
+        leServer.init()
+        advanceUntilIdle()
+        leServer.setTarget(device)
+
+        pebbleProtocol.registerReceiveCallback(ProtocolEndpoint.PHONE_VERSION) {
+            assert(it is PhoneAppVersion.AppVersionRequest)
+            pebbleProtocol.send(resp)
+        }
+
+        val defer = async {
+            protocolHandler.connectionStateChannel.receive()
+        }
+
+        watchConnection()
+        advanceUntilIdle()
+
+        scope.launch {
+            pebbleProtocol.startPacketSendingLoop {
+                protocolHandler.sendPebblePacket(it.asByteArray())
+            }
+        }
+        watchSendPacket(PhoneAppVersion.AppVersionRequest().serialize().asByteArray(), 139)
+        advanceUntilIdle()
+        Timber.d("Sent packet")
+        assert(defer.await())
+        val ackData = dataUpdates.receive()
+        assertEquals(GATTPacket.PacketType.ACK, GATTPacket(ackData).type)
+        val respData = dataUpdates.receive()
+        assertEquals(GATTPacket.PacketType.DATA, GATTPacket(respData).type)
+        val pblPacket = PebblePacket.deserialize(readPacketIn(respData))
+        assert(pblPacket is PhoneAppVersion.AppVersionResponse)
+        assertArrayEquals(resp.serialize().asByteArray(), pblPacket.serialize().asByteArray())
+    }
+
+    private suspend fun watchConnection() {
         callbacks.onCharacteristicReadRequest(device, 0, 0, BluetoothGattCharacteristic(UUID.fromString(LEConstants.UUIDs.META_CHARACTERISTIC_SERVER), 0, 0))
         callbacks.onCharacteristicWriteRequest(
                 device,
@@ -138,6 +202,31 @@ class PPoGATTProtocolHandlerTest {
                 0,
                 GATTPacket(GATTPacket.PacketType.RESET, 0, byteArrayOf(0, 0)).data
         )
+        val ack = GATTPacket(dataUpdates.receive())
+        assert(ack.type == GATTPacket.PacketType.RESET_ACK)
+        assertEquals(0, ack.sequence)
+    }
+
+    private suspend fun watchSendPacket(data: ByteArray, chunkSize: Int) {
+        val chunks = data.toList().chunked(chunkSize)
+        for (chunk in chunks) {
+            callbacks.onCharacteristicWriteRequest(
+                    device,
+                    1,
+                    BluetoothGattCharacteristic(UUID.fromString(LEConstants.UUIDs.PPOGATT_DEVICE_CHARACTERISTIC_SERVER), 0, 0),
+                    false,
+                    false,
+                    0,
+                    GATTPacket(GATTPacket.PacketType.DATA, seq, chunk.toByteArray()).data
+            )
+            seq++
+            delay(kotlin.random.Random.nextLong(50))
+        }
+    }
+
+    private suspend fun readPacketIn(firstPacket: ByteArray): UByteArray {
+        val first = GATTPacket(firstPacket)
+        require(first.type == GATTPacket.PacketType.DATA)
         callbacks.onCharacteristicWriteRequest(
                 device,
                 1,
@@ -145,10 +234,28 @@ class PPoGATTProtocolHandlerTest {
                 false,
                 false,
                 0,
-                GATTPacket(GATTPacket.PacketType.DATA, 0, byteArrayOf(0)).data
+                GATTPacket(GATTPacket.PacketType.ACK, first.sequence).data
         )
-        advanceUntilIdle()
-
-        assert(defer.await())
+        val pblData = firstPacket.drop(1).toMutableList()
+        val length = (DataBuffer(pblData.toByteArray().asUByteArray()).getUShort()+4u).toInt()
+        Timber.d("Read ${pblData.size}/$length")
+        while (pblData.size < length) {
+            val more = dataUpdates.receive()
+            val packet = GATTPacket(more)
+            check(packet.type == GATTPacket.PacketType.DATA)
+            pblData.addAll(packet.data.drop(1))
+            Timber.d("Read ${pblData.size}/$length")
+            callbacks.onCharacteristicWriteRequest(
+                    device,
+                    1,
+                    BluetoothGattCharacteristic(UUID.fromString(LEConstants.UUIDs.PPOGATT_DEVICE_CHARACTERISTIC_SERVER), 0, 0),
+                    false,
+                    false,
+                    0,
+                    GATTPacket(GATTPacket.PacketType.ACK, packet.sequence).data
+            )
+        }
+        check(pblData.size == length)
+        return pblData.toByteArray().asUByteArray()
     }
 }
