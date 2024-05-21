0ced754a09fe46d1cb84aa7e567e0e9007db3530
crc-32 <me@crc32.dev>
2024-05-21 03:49:02 +0100

    add le connector to LEDriver

--- android/pebble_bt_transport/src/main/java/io/rebble/cobble/bluetooth/ble/BlueLEDriver.kt
+++ android/pebble_bt_transport/src/main/java/io/rebble/cobble/bluetooth/ble/BlueLEDriver.kt
@@ -10,9 +10,12 @@ import io.rebble.cobble.bluetooth.SingleConnectionStatus
 import io.rebble.cobble.bluetooth.workarounds.UnboundWatchBeforeConnecting
 import io.rebble.cobble.bluetooth.workarounds.WorkaroundDescriptor
 import io.rebble.libpebblecommon.ProtocolHandler
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
+import java.io.IOException
 
 /**
  * Bluetooth Low Energy driver for Pebble watches
@@ -23,6 +26,8 @@ import kotlinx.coroutines.flow.flow
 class BlueLEDriver(
         private val context: Context,
         private val protocolHandler: ProtocolHandler,
+        private val scope: CoroutineScope,
+        private val ppogServer: PPoGService,
         private val workaroundResolver: (WorkaroundDescriptor) -> Boolean
 ): BlueIO {
     @OptIn(FlowPreview::class)
@@ -32,7 +37,16 @@ class BlueLEDriver(
         require(device.bluetoothDevice != null)
         return flow {
             val gatt = device.bluetoothDevice.connectGatt(context, workaroundResolver(UnboundWatchBeforeConnecting))
+                    ?: throw IOException("Failed to connect to device")
             emit(SingleConnectionStatus.Connecting(device))
+            val connector = PebbleLEConnector(gatt, context, scope)
+            connector.connect().collect {
+                when (it) {
+                    PebbleLEConnector.ConnectorState.CONNECTING -> Timber.d("PebbleLEConnector is connecting")
+                    PebbleLEConnector.ConnectorState.PAIRING -> Timber.d("PebbleLEConnector is pairing")
+                    PebbleLEConnector.ConnectorState.CONNECTED -> Timber.d("PebbleLEConnector connected watch, waiting for watch")
+                }
+            }
         }
     }
 }
