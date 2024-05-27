5e000338118b3f2e8463f97fc1e1dba739b61807
crc-32 <me@crc32.dev>
2024-05-27 23:19:49 +0100

    chunk only up to max size

--- android/pebble_bt_transport/src/main/java/io/rebble/cobble/bluetooth/ble/util/byteArrayChunker.kt
+++ android/pebble_bt_transport/src/main/java/io/rebble/cobble/bluetooth/ble/util/byteArrayChunker.kt
@@ -1,10 +1,12 @@
 package io.rebble.cobble.bluetooth.ble.util
 
+import kotlin.math.min
+
 fun ByteArray.chunked(size: Int): List<ByteArray> {
     val list = mutableListOf<ByteArray>()
     var i = 0
     while (i < this.size) {
-        list.add(this.sliceArray(i until (i + size)))
+        list.add(this.sliceArray(i until (min(i+size, this.size))))
         i += size
     }
     return list
