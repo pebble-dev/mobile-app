12a52b91cd15b78449528de884def05f53d2f83b
crc-32 <me@crc32.dev>
2024-08-11 17:59:28 +0100

    add token copying scary page

--- lib/ui/devoptions/debug_options_page.dart
+++ lib/ui/devoptions/debug_options_page.dart
@@ -2,13 +2,17 @@ import 'package:cobble/domain/api/auth/auth.dart';
 import 'package:cobble/domain/api/auth/user.dart';
 import 'package:cobble/domain/api/no_token_exception.dart';
 import 'package:cobble/domain/calendar/device_calendar_plugin_provider.dart';
+import 'package:cobble/domain/logging.dart';
 import 'package:cobble/infrastructure/datasources/preferences.dart';
+import 'package:cobble/infrastructure/datasources/secure_storage.dart';
 import 'package:cobble/infrastructure/datasources/web_services/auth.dart';
 import 'package:cobble/infrastructure/pigeons/pigeons.g.dart';
 import 'package:cobble/ui/common/components/cobble_button.dart';
+import 'package:cobble/ui/common/icons/fonts/rebble_icons.dart';
 import 'package:cobble/ui/router/cobble_scaffold.dart';
 import 'package:cobble/ui/router/cobble_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -105,36 +109,83 @@ class DebugOptionsPage extends HookConsumerWidget implements CobbleScreen {
             title: const Text("Enable sensitive data logging"),
             subtitle: const Text("Enables more in-depth logging at the cost of privacy (e.g. notification contents)"),
           ),
-          CobbleButton(
-            onPressed: () async {
-              try {
-                AuthService auth = await ref.read(authServiceProvider.future);
-                User user = await auth.user;
-                String id = user.uid.toString();
-                String bootOverrideCount = user.bootOverrides?.length.toString() ?? "0";
-                String subscribed = user.isSubscribed.toString();
-                String timelineTtl = user.timelineTtl.toString();
-                debug.collectLogs(
-                  """
+          Column(
+            children: [
+              const SizedBox(height: 20),
+              CobbleButton(
+                onPressed: () async {
+                  try {
+                    AuthService auth = await ref.read(authServiceProvider.future);
+                    User user = await auth.user;
+                    String id = user.uid.toString();
+                    String bootOverrideCount = user.bootOverrides?.length.toString() ?? "0";
+                    String subscribed = user.isSubscribed.toString();
+                    String timelineTtl = user.timelineTtl.toString();
+                    debug.collectLogs(
+                      """
 User ID: $id
 Boot override count: $bootOverrideCount
 Subscribed: $subscribed
 Timeline TTL: $timelineTtl
                 """,
-                );
-              } on NoTokenException catch (_) {
-                debug.collectLogs("Not logged in");
-              }
-            },
-            label: "Share application logs",
-          ),
-          const SizedBox(height: 20),
-          CobbleButton(
-            label: "Force calendar resync",
-            onPressed: () async {
-              calendarControl.requestCalendarSync(true);
-            },
-          ),
+                    );
+                  } on NoTokenException catch (_) {
+                    debug.collectLogs("Not logged in");
+                  }
+                },
+                label: "Share application logs",
+              ),
+              const SizedBox(height: 20),
+              CobbleButton(
+                label: "Force calendar resync",
+                onPressed: () async {
+                  calendarControl.requestCalendarSync(true);
+                },
+              ),
+              const SizedBox(height: 20),
+              CobbleButton(
+                label: "Copy private token",
+                onPressed: () async {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        icon: const Icon(RebbleIcons.warning),
+                        title: const Text("Security Warning"),
+                        content: const Text("This token allows full access to your Rebble account, you probably don't want to obtain it unless you're a developer."),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text("Cancel"),
+                          ),
+                          TextButton(
+                              onPressed: () async {
+                                final token = await (await ref.read(tokenProvider.future));
+                                if (token == null) {
+                                  return;
+                                }
+                                Clipboard.setData(ClipboardData(text: token.accessToken));
+                                if (!context.mounted) {
+                                  return;
+                                }
+                                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Token copied to clipboard"), duration: Duration(seconds: 2)));
+                                Navigator.of(context).pop();
+                              },
+                              style: Theme.of(context).textButtonTheme.style?.copyWith(
+                                foregroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.error),
+                              ),
+                              child: const Text("Copy")
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              )
+            ],
+          )
         ],
       ).toList()),
     );
