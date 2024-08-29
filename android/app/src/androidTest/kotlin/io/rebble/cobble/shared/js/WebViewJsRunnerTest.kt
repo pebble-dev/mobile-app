b90187274f0c9357f9797a9c1280cd7fbb485d85
crc-32 <me@crc32.dev>
2024-08-28 22:43:45 -0400

    js print test

--- android/app/src/androidTest/kotlin/io/rebble/cobble/shared/js/WebViewJsRunnerTest.kt
+++ android/app/src/androidTest/kotlin/io/rebble/cobble/shared/js/WebViewJsRunnerTest.kt
@@ -4,19 +4,41 @@ import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import io.rebble.libpebblecommon.metadata.pbw.appinfo.PbwAppInfo
 import io.rebble.libpebblecommon.util.runBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.serialization.json.Json
 import org.junit.Before
 import org.junit.Test
+import java.io.File
 
 class WebViewJsRunnerTest {
 
     private lateinit var context: Context
     private val json = Json {ignoreUnknownKeys = true}
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
     @Before
     fun setUp() {
         context = InstrumentationRegistry.getInstrumentation().targetContext
     }
+
+    // Copies from the android_assets of the test to the sdcard so the WebView can access it
+    private fun assetsToSdcard(file: String): String {
+        val sdcardPath = context.getExternalFilesDir(null)!!.absolutePath
+        val testPath = "$sdcardPath/test"
+        File(testPath).mkdir()
+        val testFile = "$testPath/$file"
+        val assetManager = InstrumentationRegistry.getInstrumentation().getContext().getAssets()
+        val inputStream = assetManager.open(file)
+        val outputStream = File(testFile).outputStream()
+        inputStream.copyTo(outputStream)
+        inputStream.close()
+        outputStream.close()
+        return testFile
+    }
+
     @Test
     fun test() = runBlocking {
         val appInfo: PbwAppInfo = json.decodeFromString(
@@ -54,9 +76,10 @@ class WebViewJsRunnerTest {
                 }
             """.trimIndent()
         )
-        val printTestPath = "file:///android_asset/print_test.js"
-        val webViewJsRunner = WebViewJsRunner(context, appInfo, printTestPath)
+        val printTestPath = assetsToSdcard("print_test.js")
+        val webViewJsRunner = WebViewJsRunner(context, MutableStateFlow("dummy"), coroutineScope, appInfo, printTestPath)
         webViewJsRunner.start()
-        delay(5000)
+        delay(1000)
+        webViewJsRunner.stop()
     }
 }
