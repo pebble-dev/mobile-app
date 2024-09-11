abf0601b45a1c21d25dc10e0809bc59e70f8b8fd
crc-32 <me@crc32.dev>
2024-09-11 01:31:32 -0400

    whatsapp call null coalesce

--- android/shared/src/androidMain/kotlin/io/rebble/cobble/shared/domain/notifications/calls/WhatsAppCallNotificationInterpreter.kt
+++ android/shared/src/androidMain/kotlin/io/rebble/cobble/shared/domain/notifications/calls/WhatsAppCallNotificationInterpreter.kt
@@ -4,9 +4,9 @@ import android.service.notification.StatusBarNotification
 
 class WhatsAppCallNotificationInterpreter: CallNotificationInterpreter {
     override fun processCallNotification(sbn: StatusBarNotification): CallNotification? {
-        val acceptAction = sbn.notification.actions.firstOrNull { it.title.toString().contains("Answer", true) }
-        val declineAction = sbn.notification.actions.firstOrNull { it.title.toString().contains("Decline", true) }
-        val hangUpAction = sbn.notification.actions.firstOrNull { it.title.toString().contains("Hang up", true) }
+        val acceptAction = sbn.notification.actions?.firstOrNull { it.title.toString().contains("Answer", true) }
+        val declineAction = sbn.notification.actions?.firstOrNull { it.title.toString().contains("Decline", true) }
+        val hangUpAction = sbn.notification.actions?.firstOrNull { it.title.toString().contains("Hang up", true) }
 
         if (acceptAction != null && declineAction != null) {
             val contactName = sbn.notification.extras.getCharSequence("android.title")?.toString()?.let {
