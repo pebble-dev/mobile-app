5db690ae768a9732a220dc933d38ebd1e052eed2
crc-32 <me@crc32.dev>
2024-10-20 16:59:55 +0100

    start implementing rws ASR client

---
+++ android/shared/src/commonMain/kotlin/io/rebble/cobble/shared/api/ASRClient.kt
@@ -0,0 +1,65 @@
+package io.rebble.cobble.shared.api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.headers
+import io.ktor.client.request.post
+import io.ktor.client.utils.EmptyContent.headers
+import io.ktor.utils.io.reader
+import io.ktor.utils.io.writer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class ASRClient(
+        val baseUrl: String,
+        private val token: String
+): KoinComponent {
+    private val client: HttpClient by inject()
+    private val asrInfo = "{}"
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    fun createStreamingSession(block: suspend StreamingSessionScope.() -> Unit) = flow {
+        val channel = Channel<ByteArray>(capacity = Channel.BUFFERED)
+        val session = StreamingSessionScope(channel)
+        val request = client.post("$baseUrl/NmspServlet/") {
+            headers {
+                append("Connection", "Keep-Alive")
+                append("Authorization", "Bearer $token")
+            }
+        }
+        request.writer {
+            channel.send(asrInfo.encodeToByteArray())
+            channel.consumeEach { frameData ->
+                channel.send(frameData)
+            }
+        }
+        block(session)
+        channel.send("--boundary--\r\n".encodeToByteArray())
+        //TODO: Implement response handling
+    }
+
+    class StreamingSessionScope(private val channel: Channel<ByteArray>) {
+        suspend fun writeFrames(frameData: ByteArray) {
+            writePart(frameData, "ConcludingAudioParameter", "AUDIO_INFO", "audio/x-speex-with-header-byte")
+        }
+
+        private suspend fun writePart(data: ByteArray, name: String, paramName: String, contentType: String) {
+            val part = """
+                --boundary
+                Content-Disposition: form-data; name="$name"${if (paramName.isNotEmpty()) "; paramName=\"$paramName\"" else ""}
+                Content-Type: $contentType
+                Content-Transfer-Encoding: 8bit
+                
+            """.trimIndent()
+            channel.send(part.encodeToByteArray())
+            channel.send(data)
+            channel.send("\r\n".encodeToByteArray())
+        }
+    }
+}
