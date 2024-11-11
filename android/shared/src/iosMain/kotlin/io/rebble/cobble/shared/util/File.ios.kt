55a4aaa3df2e1f88a2a30717a276d7073a4ce83b
crc-32 <me@crc32.dev>
2024-11-11 19:49:24 +0000

    clean up iOS placeholder files

--- android/shared/src/iosMain/kotlin/io/rebble/cobble/shared/util/File.ios.kt
+++ android/shared/src/iosMain/kotlin/io/rebble/cobble/shared/util/File.ios.kt
@@ -1,7 +1,13 @@
 package io.rebble.cobble.shared.util
 
+import io.ktor.utils.io.ByteReadChannel
+
 actual class File actual constructor(uri: String) {
     actual fun exists(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    actual fun readChannel(): ByteReadChannel {
         TODO("Not yet implemented")
     }
 }
